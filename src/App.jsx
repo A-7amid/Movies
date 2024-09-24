b945b8685581b3e36a7d45c4af76719f0691aa0a
Hi <hello@ww.com>
2024-09-24 22:21:36 +0300

    First Version

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import SearchIcon from './search.svg'
 import MovieCard from "./MovieCard"
 
-const API_URL = 'http://www.omdbapi.com/?apikey=a0427c75'
+const API_URL = 'https://www.omdbapi.com/?apikey=a0427c75'
 
 // const movie1 = {
 //   "Title": "Batman v Superman: Dawn of Justice",
